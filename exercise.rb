0135adfc5abff75d7f0283c01a20172ad4568e70
Marlon O'Neil <hello@marlononeil.com>
2017-08-16 14:01:52 -0400

    completed exercise 1

---
+++ exercise.rb
@@ -0,0 +1,10 @@
+#Exercise 1
+
+# Create an emotions hash, where the keys are the names of different human emotions and the values are the degree to which the emotion is being felt on a scale from 1 to 3.
+
+emotions = {
+  :happy => 1,
+  :sad => 3,
+  :excited => 2,
+  :angry => 1
+}
