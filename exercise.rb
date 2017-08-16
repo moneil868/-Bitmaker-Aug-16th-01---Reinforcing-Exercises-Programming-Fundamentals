4062e68fa76248b014a1171b769738d7c7644d84
Marlon O'Neil <hello@marlononeil.com>
2017-08-16 14:11:28 -0400

    completed exercise 2

--- exercise.rb
+++ exercise.rb
@@ -1,10 +1,38 @@
 #Exercise 1
+  # Create an emotions hash, where the keys are the names of different human emotions and the values are the degree to which the emotion is being felt on a scale from 1 to 3.
 
-# Create an emotions hash, where the keys are the names of different human emotions and the values are the degree to which the emotion is being felt on a scale from 1 to 3.
-
-emotions = {
+emotions_hash = {
   :happy => 1,
   :sad => 3,
   :excited => 2,
   :angry => 1
 }
+
+
+#Exercise 2
+  #Write a Person class with the following characteristics:
+
+class Person
+
+  def initialize(name)
+    # name (string)
+    @name = name
+
+    #emotions (hash)
+    @emotions = {}
+  end
+
+  def add_emotions(emotions)
+    @emotions = emotions
+    # puts emotions
+  end
+
+end
+
+
+#Initialize an instance of Person using your emotions hash from exercise 1.
+first_person = Person.new("Marlon")
+puts first_person.inspect
+
+first_person.add_emotions(emotions_hash)
+puts first_person.inspect
